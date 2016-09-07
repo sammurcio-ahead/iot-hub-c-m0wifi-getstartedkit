58da7af837740dc029e0b721d6a8b057abfd11c5
Ilias Tsigkogiannis <iliast@microsoft.com>
2016-09-07 15:58:17 -0700

    Fixed stale values in app

--- command_center_node/server.js
+++ command_center_node/server.js
@@ -58,7 +58,7 @@ app.get('/api/alerts', function(req, res) {
 app.get('/api/temperatures', function(req, res) {
     var query = new azure.TableQuery()
         .select(['eventtime', 'temperaturereading', 'deviceid'])
-        .where('PartitionKey eq ?', deviceId);
+        .where('PartitionKey eq ? and eventtime >= ?', deviceId, Math.floor(Date.now()/1000) - 1000);
     tableSvc.queryEntities(storageTable, query, null, function(err, result, response) {
         res.json(result.entries.slice(-10));
     })
